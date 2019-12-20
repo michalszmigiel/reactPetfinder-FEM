932f1df5fc8935849634d0ae17ecc8203b71ae4c
Michał R. Szmigiel <michal.r.szmigiel@gmail.com>
2019-12-20 09:03:40 +0100

    prettier and eslint config

---
+++ src/App.js
@@ -0,0 +1,39 @@
+const Pet = props => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
+  ]);
+};
+
+class App extends React.Component {
+  handleTitleClick() {
+    alert("you clicked the title");
+  }
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Petfinder"
+      ),
+      React.createElement(Pet, {
+        name: "Juniś",
+        animal: "dog",
+        breed: "pintcher"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, {
+        name: "Doink",
+        animal: "cat",
+        breed: "mixed"
+      })
+    ]);
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));

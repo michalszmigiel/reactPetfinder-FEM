00a961536ed2ca52e096b2f4a66a0e51eed0c377
Michał R. Szmigiel <michal.r.szmigiel@gmail.com>
2019-12-20 11:58:06 +0100

    fetching data from api

--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,34 @@
 import React from "react";
 
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name.toUpperCase()}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
-};
+class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+
+    let photos = [];
+
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+
+    let imgReplacement = "http://placecorgi.com/300/300";
+    if (photos[0] && photos[0].value) {
+      imgReplacement = photos[0].value;
+    }
+
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={imgReplacement} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {location}
+          </h2>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Pet;
